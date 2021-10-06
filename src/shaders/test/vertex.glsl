ba141598834b361f1af4cc187524ca7b61d9a111
Samir Ghosh <hello@samir.tech>
2021-10-05 17:13:37 -0700

    fix error with starter

--- src/shaders/test/vertex.glsl
+++ src/shaders/test/vertex.glsl
@@ -2,8 +2,8 @@ uniform float uTime;
 
 void main() {    
     vec4 modelPosition = modelMatrix * vec4(position, 1.0);
-    vec4 viewPosition = viewMatrix * newModelPosition;
+    vec4 viewPosition = viewMatrix * modelPosition;
     vec4 projectedPosition = projectionMatrix * viewPosition;
     gl_Position = projectedPosition;
-    gl_PointSize = 5.0;
+    gl_PointSize = 10.0;
 }
