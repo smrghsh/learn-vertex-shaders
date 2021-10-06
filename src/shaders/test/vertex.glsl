97040acdb89901c4c65caeea4df6e62cfd4eae9c
Samir Ghosh <hello@samir.tech>
2021-10-05 17:02:54 -0700

    hide the answer

--- src/shaders/test/vertex.glsl
+++ src/shaders/test/vertex.glsl
@@ -2,8 +2,6 @@ uniform float uTime;
 
 void main() {    
     vec4 modelPosition = modelMatrix * vec4(position, 1.0);
-    vec4 newModelPosition = modelPosition;
-    newModelPosition.y += 2. * sin(uTime + newModelPosition.x/3.0);
     vec4 viewPosition = viewMatrix * newModelPosition;
     vec4 projectedPosition = projectionMatrix * viewPosition;
     gl_Position = projectedPosition;
